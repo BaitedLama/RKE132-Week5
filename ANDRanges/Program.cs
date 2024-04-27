a6d2b0ce4946327fa5a67012f9c170944ba9d12d
BaitedLama <kaspar.kool.kask@gmail.com>
2024-04-27 14:44:38 +0300

    first commit

---
+++ ANDRanges/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter your temperature:");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp >= 30)
+{
+    Console.WriteLine("Hot.");
+}
+else if (temp < 20 && temp >= 15)
+{
+    Console.WriteLine("Warm.");
+}
+else if (temp < 15 && temp >=10)
+{
+    Console.WriteLine("Chilly.");
+}
+else if (temp < 10 && temp > 0 )
+{
+    Console.WriteLine("Cold.");
+}
+else
+{
+    Console.WriteLine("Freezing cold.");
+}
